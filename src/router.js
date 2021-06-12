27335ca7ff7c03597b72068e72734f4c96a78a34
Mohdan <mohdan.barzankaev@gmail.com>
2021-06-12 22:00:33 +0300

    before/each/enter/leave/routeEnter/routeLeave, afterEach, name

--- src/router.js
+++ src/router.js
@@ -6,13 +6,19 @@ import Mail from './views/Mail'
 import AppEmailBody from './components/AppEmailBody'
 import NotFound from './views/NotFound'
 
-export default createRouter({
+const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/login', component: Login, alias: '/' },
-        { path: '/forget', component: Forget },
-        { path: '/dashboard', component: Dashboard },
-        { path: '/mail', component: Mail, children: [
+        { path: '/forget', component: Forget, meta: {cantEnter: true}},
+        { 
+            path: '/dashboard', 
+            component: Dashboard, 
+            name: 'home', 
+            beforeEnter() {
+                console.log('befforeEnter')
+            } },
+        { path: '/mail', component: Mail, name: 'email', children: [
             { path: ':mailId?', component: AppEmailBody, props: true }
         ]},
         { path: '/:notFound(.*)', component: NotFound }
@@ -20,3 +26,18 @@ export default createRouter({
     linkActiveClass: 'active',
     linkExactActiveClass: 'active'
 })
+
+router.beforeEach((to, from, next) => {
+    console.log('beforeEach')
+    if(to.meta.cantEnter) {
+        next({name: 'home'})
+    }else {
+        next()
+    }
+})
+
+router.afterEach(() => {
+    console.log('afterEach')
+})
+
+export default router
